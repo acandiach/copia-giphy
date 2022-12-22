bfe667a68cde48893bef14d7f3178a78839769c8
acandia <abelcandiach@gmail.com>
2022-12-22 17:20:10 -0300

    feat:test con minitest agregado

--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class PublicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'No se puede guardar la publicacion sin texto' do
+    products(:publication_one, :publication_two).each do |publication|
+      res = publication.valid?
+      assert_equal false, res, "No estan validando el dato #{publication.title}"
+    end
+  end
 end
